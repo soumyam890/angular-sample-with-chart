3b585cbed63b452a62622a0f4812a74ac9cb65e9
Soumya Mohan <soumyam890@gmail.com>
2023-05-10 12:54:58 +0300

    more changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TreeNode } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-sample-with-chart';
+
+  selectedNodes: TreeNode[] | undefined;
+  chips: string[] = ['Chip 1', 'Chip 2', 'Chip 3'];
+
+  data: TreeNode[] = [
+    {
+      expanded: true,
+      type: 'person',
+      data: {
+        image: 'https://primefaces.org/cdn/primeng/images/demo/avatar/amyelsner.png',
+        title: 'CEO',
+        mood: 'happy',
+        features: [{ 'name': 'burnouts', 'value': 'High' },
+        { 'name': 'WLB', 'value': 'Low' },
+        { 'name': 'Flight Risk', 'value': 'High' },
+        { 'name': 'Utilization', 'value': 'Low' }]
+      },
+      children: [
+        {
+          expanded: true,
+          type: 'person',
+          data: {
+            image: 'https://primefaces.org/cdn/primeng/images/demo/avatar/annafali.png',
+            title: 'CMO',
+            mood: 'sad',
+            features: [{ 'name': 'burnouts', 'value': 'High' },
+            { 'name': 'WLB', 'value': 'High' },
+            { 'name': 'Flight Risk', 'value': 'High' },
+            { 'name': 'Utilization', 'value': 'High' }]
+          }
+        },
+        {
+          expanded: true,
+          type: 'person',
+          data: {
+            image: 'https://primefaces.org/cdn/primeng/images/demo/avatar/stephenshaw.png',
+            title: 'CTO',
+            mood: 'neutral',
+            features: [{ 'name': 'burnouts', 'value': 'Low' },
+            { 'name': 'WLB', 'value': 'High' },
+            { 'name': 'Flight Risk', 'value': 'Low' },
+            { 'name': 'Utilization', 'value': 'High' }]
+          }
+        }
+      ]
+    }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrganizationChartModule } from 'primeng/organizationchart';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon'; 
 
 @NgModule({
   declarations: [
@@ -10,6 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    OrganizationChartModule,
+    MatChipsModule,
+    MatIconModule,
     AppRoutingModule
   ],
   providers: [],
